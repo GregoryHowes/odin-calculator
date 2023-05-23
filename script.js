9c050b83e5290593b378f21705b1dbf4395890a6
Greg Howes <gihowes@gmail.com>
2023-05-23 09:53:55 +0900

    Add clear button funtionality Plus fixed input logic around zeros and decimal points

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //define constants
-const maxDigits = 10;
+const maxDigits = 12;
 
 //define global variables
 let currentDigits = "0";
@@ -10,14 +10,21 @@ usedDecimalPoint = false;
 const numberDisplay = document.querySelector(".readout");
 numberDisplay.innerText = currentDigits;
 
-
+//--------------------------
 // Create listeners for keys
 const numberButtons = Array.from(document.querySelectorAll('.btn-num'));
-  numberButtons.forEach(numberButton => numberButton.addEventListener('click', inputNumber));
+numberButtons.forEach(numberButton => numberButton.addEventListener('click', inputNumber));
+
+const btnClear = document.querySelector("#clear");
+btnClear.addEventListener("click", clearDisplay);
+
+//--------------------------
+
+
 
 function inputNumber(e) {
     //check that there is space to add extra digits
-    if (currentDigits.length <10) {
+    if (currentDigits.length < maxDigits) {
         if (e.srcElement.innerText === ".") {
             if (!usedDecimalPoint) {
                 updateDisplay(e.srcElement.innerText);
@@ -31,17 +38,24 @@ function inputNumber(e) {
     console.log(e.srcElement.innerText);
 }
 
-
 function updateDisplay(digit) {
     //if display is still zero then set it to blank
-    if (currentDigits === "0") {
-        if (digit = ".") {
+    if (currentDigits == "0") {
+        if (digit == ".") {
             currentDigits = "0.";
         } else {
-            currentDigits = "";
+            if (digit !== "0" && digit != "00") {
+                currentDigits = digit;
+            }
         }
     } else {
         currentDigits += digit;
     }
     numberDisplay.innerText = currentDigits;
+}
+
+function clearDisplay() {
+    currentDigits = "0";
+    numberDisplay.innerText = currentDigits;
+    usedDecimalPoint = false;
 }
